760dc58d6ca030004bbeec221edf32b9818dc548
sumayya <sumayya.kareem6@gmail.com>
2022-08-04 20:24:51 +0530

    Update productdata.js

--- src/model/productdata.js
+++ src/model/productdata.js
@@ -1,5 +1,11 @@
 const mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost:27017/ProductDb');
+// mongoose.connect('mongodb://localhost:27017/ProductDb');
+mongoose.connect(`mongodb+srv://2WQ9IurjqctRc5zM:< 2WQ9IurjqctRc5zM  >@cluster0.3nral.mongodb.net/?retryWrites=true&w=majority`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true //this line is not mandatory
+}).then(()=>{
+    console.log('Connected to MongoDB Cloud :)');
+})
 const Schema=mongoose.Schema;
 var NewProductSchema=new Schema({
     productId : Number,
