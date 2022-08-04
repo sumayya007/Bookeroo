682dbf568b6ccf43122a793e422846d63fd71f63
sumayya <sumayya.kareem6@gmail.com>
2022-08-04 21:24:19 +0530

    Update bookuserdata.js

--- src/model/bookuserdata.js
+++ src/model/bookuserdata.js
@@ -1,11 +1,12 @@
 const mongoose =require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/ProductDb');
-mongoose.connect(`mongodb+srv://2WQ9IurjqctRc5zM:%3Cmymongodbatlas007%3E@cluster0.3nral.mongodb.net/test`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true //this line is not mandatory
-}).then(()=>{
-    console.log('Connected to MongoDB Cloud :)');
-})
+// mongoose.connect(`mongodb+srv://2WQ9IurjqctRc5zM:%3Cmymongodbatlas007%3E@cluster0.3nral.mongodb.net/test`,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true //this line is not mandatory
+// }).then(()=>{
+//     console.log('Connected to MongoDB Cloud :)');
+// })
+mongoose.connect('mongodb+srv://2WQ9IurjqctRc5zM:%3Cmymongodbatlas007%3E@cluster0.3nral.mongodb.net/test');
 const Schema=mongoose.Schema;
 var NewUserSchema=new Schema({
     username :String,
